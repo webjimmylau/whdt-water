dadba08b6a18aab5051b0c3a3bc98ea0aa98d9bc
刘志敏 <463404548@qq.com>
2022-12-02 17:04:28 +0800

    init element-plus

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import ElementPlus from "element-plus";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+
+const app = createApp(App);
+
+// 中文配置
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+
+app.mount("#app");
